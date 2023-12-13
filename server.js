bda2a5b01d825ded68ebfb531d6eb5467c200a14
Inchree <Vatcharapong.j@hotmail.com>
2023-12-13 11:05:55 +0700

    Start graphQL

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = buildSchema(`
+    type Query {
+        product: [Product]
+        orders: [Order]
+    }
+
+    type Product {
+        id: ID!
+        description: String!
+        price: Float!
+        reviews: [Review]
+    }
+
+    type Review {
+        rating: Int!
+        comment: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
+    }
+`);
+
+const root = {
+    products: [
+        {
+            id: 'redshoe',
+            description: 'Red Shoe',
+            price: 42.12,
+        },{
+            id: 'bluejean',
+            description: 'Blue Jeans',
+            price: 60
+        }
+    ],
+    orders: [
+        {
+            date: '2005-05-05',
+            subtotal: 90.22,
+            items: [
+                {
+                    product: {
+                        id: 'redshoe',
+                        description: 'Old Red Shoe',
+                        price: 45.11,
+                    },
+                    quantity: 2,
+                }
+            ]
+        }
+    ]
+}
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}))
+
+app.listen(3000, () => {
+    console.log(`Running server GraphQL server...`)
+})
